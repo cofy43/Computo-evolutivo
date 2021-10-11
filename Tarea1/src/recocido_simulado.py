201b8dcf430454ed6083973c6db56a5b1a7bf7d7
Martin Felipe <cofy43@ciencias.com>
2021-10-11 18:32:24 -0500

    get params to files

---
+++ Tarea1/src/recocido_simulado.py
@@ -0,0 +1,29 @@
+import sys
+
+casos = 0
+capacidad = 0
+elementos = []
+
+def read_bin_file(fileName):
+    try:
+        file = open(fileName, "r")
+    except OSError:
+        print("No se pudo leer el archivo: " + fileName)
+        print("Verifica que el nombre del archivo es correcto o existe")
+        print("e intentalo nuevamente")
+        sys.exit()
+    with file:
+        lines = file.readlines()
+        parametros = lines.pop(0).replace("\n", "").split(" ")
+        casos = parametros[0]
+        capacidad = parametros[1]
+        for line in lines:
+            elementos.append(line.replace("\n", "").split(" "))
+            
+        print("casos: ", casos)
+        print("capacidad: ", capacidad)
+        print("elementos: ", elementos)
+        file.close()
+        
+
+read_bin_file("../bin/ks_4_0")
