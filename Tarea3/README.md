cd0ebf61bca9f899dcb6c4a7dea39fcfb25bd137
Martin Felipe <cofy43@ciencias.com>
2021-11-26 16:15:39 -0600

    definicon de problemas

---
+++ Tarea3/README.md
@@ -0,0 +1,21 @@
+## Tarea 3
+Desarrollar un algoritmo evolutivo en Python con las siguientes características:
+ * Seleccionar los componentes básicos del evolutivo
+ * Seleccionar al menos dos técnicas avanzadas
+    * Paralelismo
+    * Coevolución
+    * Metamodelos
+    * Técnicas de diversidad
+    * Meméticos
+    * Hiper heurísticas 
+ * Aplicar el evolutivo a los siguientes 5 problemas con n=10
+    * Rastrigin --
+    * Ackley --
+    * Rosenbrock --
+    * Eggholder --
+    * Easom --
+ * Realizar un estudio comparativo entre el algoritmo desarrollado en la tarea 2 y el diseñado en está tarea utilizando hasta 10mil evaluaciones de función utilizando la prueba de Wilcoxon rank sum
+ * Los parametros del nuevo evolutivo deben estar optimizados con iRace
+ *  Resultados promediados de 20 ejecuciones del algoritmo. Soluciones mínima, media, máxima y desviación estándar. Usar mismos parámetros para las 20 ejecuciones y reportarlos. Se debe generar un archivo por cada ejecución del algoritmo
+ * Reportar gráfica de convergencia. Eje x número de generaciones, eje y mediana de la mejor aptitud de cada generación
+ * PDF con los resultados de la ejecución mínima, resultados promediados, gráfica de convergencia y estimación de tamaño del espacio de búsqueda
---
+++ Tarea3/problemas.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+# Número de dimensiones
+d = 10
+pi = np.pi
+
+def rastrigin(x):
+    """
+    Problema Rastrigin
+    Función definida para d dimensiones en donde 
+    los mínimos locales se encuentran distribuidos
+    uniformemente y el objetivo de esta función es
+    encontrar un mínimo obtimo
+    """
+    return 10*d + np.sum(x**2 - 10*np.cos(2*pi*x))
+
+def ackley(x):
+    """
+    Problema ACKLEY
+    Función que se caracteriza por tener una región
+    plana y un gran agujero en el centro donde se
+    encuentra el mínimo global.
+    Valores utilizados: a = 20, b = 0.2, c = 2 * pi
+    """
+    sumando1 = -20 * np.exp(-0.2 * np.sqrt(0.5 * np.sum(x**2)))
+    sumando2 = np.exp(0.5*np.sum(np.cos(2*pi*x)))
+    return  sumando1 - sumando2 + 20 + np.exp(1)
+
+def rosenbrock(x):
+    """
+    Problema ROSENBROCK
+    Función con aspecto similar al de una banana
+    con la característica de ser unimodal donde
+    el mínimo global se encuentra en un valle
+    parabólico
+    """
+    suma = 0
+    for i in range(len(x)-1):
+        xi = x.item(i)
+        xi1 = x.item(i+1)
+        suma += 100*((xi1 - (xi**2))**2) + (xi-1)**2
+    return suma
+
+def eggholder(x):
+    """
+    Problema EGGHOLDER
+    Función con un gran número de mínimos locales 
+    """
+    x1 = x[0]
+    x2 = x[1]
+    sumando1 = (x2 + 47)*np.sin(np.sqrt(np.absolute(x2 + (x1/2) + 47)))
+    sumando2 = x1*np.sin(np.sqrt(np.absolute(x1-(x2+47))))
+    return - sumando1 - sumando2
+
+def easom(x):
+    """
+    Problema EASOM
+    """
+    x1 = x[0]
+    x2 = x[0]
+    return - np.cos(x1)*np.cos(x2)*np.exp(-(x1 - np.pi)**2 - (x2 - np.pi)**2)
+
+x = np.array([1,2,3,4,5,6,7,8,9,0])
+print("rastrigin")
+print(rastrigin(x))
+print("ackley")
+print(ackley(x))
+print("rosenbrock")
+print(rosenbrock(x))
+print("eggholder")
+print(eggholder(x))
+print("easom")
+print(easom(x))
